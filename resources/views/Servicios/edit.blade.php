24500559765bbf188f3e8a9e08b16e6adc20b413
Juan Tirado <juan.tirado@unmsm.edu.pe>
2021-01-06 07:14:18 -0500

    Se arreglo el edit

--- resources/views/Servicios/edit.blade.php
+++ resources/views/Servicios/edit.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <form action="{{ url('/servicios/'.$servicios->id)}}" method="post" enctype="multipart/form-data">
+        <form action="{{ url('/servicios/'.$servicio->id)}}" method="post" enctype="multipart/form-data">
             @csrf
             {{method_field('PATCH')}}
             @include('servicios.form',['modo'=>'Editar'])
         </form>
     </div>
 @endsection
+ 
